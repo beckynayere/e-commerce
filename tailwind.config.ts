cd78a60454801253fb5dafe6e3cfab3c1deb416c
beckynayere <rebecca.nayere@gmail.com>
2025-02-19 15:40:30 +0300

    create tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,38 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  
+  plugins: [
+    
+    require('daisyui'),],
+
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+            
+  "primary": "#ff00d3",
+            
+  "secondary": "#009000",
+            
+  "accent": "#00f843",
+            
+  "neutral": "#0d1800",
+            
+  "base-100": "#223227",
+            
+  "info": "#1c98ff",
+            
+  "success": "#3bb700",
+            
+  "warning": "#ca0000",
+            
+  "error": "#ff4463",
+  body: {
+    "background-color": "#0d1800",
   },
-  plugins: [],
+            },
+          },
+        ],
+      },
 } satisfies Config;
